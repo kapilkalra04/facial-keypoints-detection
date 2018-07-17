3f2f0a3680d12a7929a27057512ee4bfa0b452aa
kapilkalra04 <kapilkalra04@gmail.com>
2018-07-17 17:41:13 +0530

    Updated ann.py to extract the input images and output labels as np arrays for training

--- src/ann.py
+++ src/ann.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import BatchNormalization, Conv2D, Activation, MaxPooling2D, Dense, GlobalAveragePooling2D
+
 
 # creating a data-frame to load the training.csv data
 print "[INFO] Reading training.csv"
 df = pd.read_csv("data/training.csv")
 
 # extracting a dataframe with only important facial landmarks
-df2 = df.ix[:,['left_eye_center_x','left_eye_center_y','right_eye_center_x','right_eye_center_y'
-,'nose_tip_x','nose_tip_y','mouth_center_bottom_lip_x','mouth_center_bottom_lip_y','Image']]
+df2 = df.ix[:,['left_eye_center_x','left_eye_center_y','right_eye_center_x',
+'right_eye_center_y','nose_tip_x','nose_tip_y','mouth_center_bottom_lip_x',
+'mouth_center_bottom_lip_y','Image']]
 
 # dropping rows with incomplete data
-df3 = df2.dropna()
+df3 = df2.dropna() 							# total no of rows in df3 = 7000 
 
 # creating a list that contains all the images but as strings
 imageStringList = []
@@ -20,8 +24,23 @@ imageStringList.extend(df3.loc[:,"Image"])
 # converting the strings to numpy arrays
 print "[INFO] Converting string enteries to images"
 imageList = []
+index=0
 for s in imageStringList:
 	imageList.append(np.array([int (e) for e in s.split(" ")]).reshape((96,96)))
+	index = index + 1
+	if index%500 == 0 :
+		print index	
 
-plt.imshow(imageList[1],cmap='gray')
-plt.show()
+# preparing the training data
+# INPUT
+X_temp = np.stack(imageList,axis=0)
+X = X_temp.astype(np.float)[:,:,:,np.newaxis] 			# X.shape = (7000,96,96,1)
+
+# OUTPUT
+Y = np.stack(df3.ix[:,[0,1,2,3,4,5,6,7]].values,axis=0) 	# Y.shape(7000,8)
+
+# scaling input range to [0,1] from [0,255]
+X = X/255
+
+# scaling target values to [-1,1] from [0,95]
+Y = (Y - 48)/48
